6e472849db6798127151fe6e8edce390fb32b6a9
Felix Odete <felix.odette@gmail.com>
2022-09-19 14:43:10 +0300

    Update: groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,4 +3,23 @@ class GroupsController < ApplicationController
     @user = current_user
     @groups = @user.groups
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user_id = current_user.id
+
+    render :new unless @group.save
+
+    redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
