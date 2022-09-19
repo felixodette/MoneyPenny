ccf202e202ff3231afc6d38e5e5ef087348a19ee
Felix Odete <felix.odette@gmail.com>
2022-09-19 14:35:45 +0300

    Update: groups, flash and navbar

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,6 @@
 class GroupsController < ApplicationController
+  def index
+    @user = current_user
+    @groups = @user.groups
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'groups#index'
+  resources :groups, only: [:index, :show, :new, :create, :destroy] do
+    resources :spendings, only: [:index]
+  end
 
+  resources :spendings, only: [:new, :create, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
