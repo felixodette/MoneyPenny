5e9cd10731891c8a5e66bc4cf9f41f8af9ab8c57
Felix Odete <felix.odette@gmail.com>
2022-09-19 13:52:54 +0300

    Bug fix: router.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
   root 'groups#index'
